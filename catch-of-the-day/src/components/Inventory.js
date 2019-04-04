9be4e82c23d0473a816ab4abe7cbb3f49c201637
Alexander Swensen <alex.swensen@gmail.com>
2019-04-03 21:55:58 -0500

    part way through authentication tutorial

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,9 +1,32 @@
 import React from "react";
 import AddFishFrom from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
+import Login from "./Login";
+import firebase from "firebase";
+import { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
+  authHandler = async authData => {
+    console.log(authData);
+  };
+
+  authenticate = provider => {
+    let authProvider;
+    if (provider === "Github") {
+      authProvider = new firebase.auth.GithubAuthProvider();
+    } else if (provider === "Twitter") {
+      authProvider = new firebase.auth.TwitterAuthProvider();
+    } else if (provider === "Facebook") {
+      authProvider = new firebase.auth.FacebookAuthProvider();
+    }
+
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
   render() {
+    return <Login authenticate={this.authenticate} />;
     return (
       <div className="inventory">
         <h2>Inventory</h2>
