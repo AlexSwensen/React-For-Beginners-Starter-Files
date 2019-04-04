3a2a808cb99cec9223d46b11a49d4e876e9347f6
Alexander Swensen <alex.swensen@gmail.com>
2019-04-04 08:51:09 -0500

    finish the site

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -3,11 +3,35 @@ import AddFishFrom from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
 import Login from "./Login";
 import firebase from "firebase";
-import { firebaseApp } from "../base";
+import base, { firebaseApp } from "../base";
 
 class Inventory extends React.Component {
+  state = {
+    uid: null,
+    owner: null
+  };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.authHandler({ user });
+      }
+    });
+  }
+
   authHandler = async authData => {
-    console.log(authData);
+    const store = await base.fetch(this.props.storeId, { context: this });
+    if (!store.owner) {
+      await base.post(`${this.props.storeId}/owner`, {
+        data: authData.user.uid
+      });
+    }
+
+    this.setState({
+      uid: authData.user.uid,
+      owner: store.owner || authData.user.uid
+    });
+    console.log(store);
   };
 
   authenticate = provider => {
@@ -25,11 +49,32 @@ class Inventory extends React.Component {
       .signInWithPopup(authProvider)
       .then(this.authHandler);
   };
+
+  logout = async () => {
+    await firebase.auth().signOut();
+    this.setState({ uid: null });
+  };
+
   render() {
-    return <Login authenticate={this.authenticate} />;
+    const logout = <button onClick={this.logout}> Log Out!</button>;
+
+    if (!this.state.uid) {
+      return <Login authenticate={this.authenticate} />;
+    }
+
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry you are not the owner!</p>
+          {logout}
+        </div>
+      );
+    }
+
     return (
       <div className="inventory">
         <h2>Inventory</h2>
+        {logout}
         {Object.keys(this.props.fishes).map(key => (
           <EditFishForm
             fish={this.props.fishes[key]}
